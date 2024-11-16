cf74fd376d0786f0f5bf180082b2ec375a0fda54
abdulladone94 <abdulladone94@gmail.com>
2024-11-16 13:41:01 +0530

    handle error usign express next

--- routes/product.js
+++ routes/product.js
@@ -15,4 +15,16 @@ router.get("/find/:id", getProduct);
 router.put("/:id", updateProduct);
 router.delete("/:id", deleteProduct);
 
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "something went wrong";
+
+  res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
+
 export default router;
